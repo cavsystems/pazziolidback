9e656f781cab12ae98fd1497238df5841f7630d8
cavsystems <desarrollo@cavsystems.com.co>
2025-05-21 10:53:34 -0500

    nuevocommit0.3

--- src/app.js
+++ src/app.js
@@ -69,7 +69,7 @@ app.use("/api", routerauth);
 app.get("/api/traerempresas", async (req, res) => {
   try {
     const { documento } = req.query;
-    if (/^\d*$/.test(documento)) {
+    if (/^\d+$/.test(documento)) {
       let datos = await dbs.sequelize.query(
         "call BuscarEmpresaPorVendedor(?)",
         {
